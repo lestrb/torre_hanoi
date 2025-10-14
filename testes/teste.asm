bbe0bcef3b234005c2c0a7fda281e8d8427d5faf
Letícia Staudinger <lsr@cin.ufpe.br>
2025-10-14 19:23:37 -0300

    Caso em que é 0

--- testes/teste.asm
+++ testes/teste.asm
@@ -1,6 +1,6 @@
 ; Hanoi
-; ah -> número do input em ascii
-; al -> número do input "normal"
+; al -> número do input em ascii
+; ah -> número do input "normal"
 ; rbx -> mensagem a ser printada
 ; rcx -> tamanho da mensagem
 
@@ -20,6 +20,7 @@ section .data
 section .bss
     numero resb 1
     ;numero_ascii resb 2     ; caractere + '\n'
+    pilha resb 4096         ; reserva 4 KB para a pilha
 
 section .text
     global _start
@@ -33,9 +34,13 @@ _print:
     ret
 
 _hanoi:
-
+    cmp ah, 0               ; checa se input foi 0 (0 discos)
+    jz fim
 
 _start:
+    ; Inicializa rsp para o topo da pilha
+    lea rsp, [pilha + 4096]  ; pilha cresce para baixo (lea calcula o endereço e coloca em rsp)
+
     ; output pedindo número de discos
     mov rbx, msg_discos
     mov rcx, msg_discos_len
@@ -52,18 +57,21 @@ _start:
     mov al, [numero]        ; al guarda caractere em ascii (pro print)
     mov ah, [numero]        
     sub ah, '0'             ; converte em numero e fica em ah (pra decrementar e incrementar)
-    
+
+    ; checa se input (ah) foi diferente de 0 pra printar frase e seguir
+    cmp ah, 0               
+    jz fim                  ; se ah == 0, pula direto para fim e não printa frase 
+
     ; output indicando número de discos
     mov byte [msg_inicial + 32], al   ; substitui o 'X' pelo número 
     mov rbx, msg_inicial
     mov rcx, msg_inicial_len
     call _print
 
+    ; chama função hanoi
+    call _hanoi
 
-
-
-    syscall
-    
+fim:    
     ; exit system call
     mov rax, 60         ; sys_exit
     mov rdi, 0          ; exit status
